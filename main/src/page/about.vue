f575c4bb990480bf6c834da43b71c2594f62cd51
yanguanghao <yanguanghao@qihangxingchen.com>
2021-12-15 13:00:11 +0800

    手动加载

---
+++ main/src/page/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about-new">
+    <div ref="container"></div>
+  </div>
+</template>
+<script>
+import { loadMicroApp } from 'qiankun';
+
+export default {
+  mounted() {
+    console.log(this.$refs['container'])
+    this.microApp = loadMicroApp({
+      name: 'app-manual',
+      entry: '//localhost:8085',
+      container: this.$refs['container'],
+      props: { brand: 'qiankun' },
+    })
+  },
+  beforeDestroy () {
+    this.microApp.unmount()
+  }
+}
+</script>
+<style>
+
+</style>
